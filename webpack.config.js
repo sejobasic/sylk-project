9464607b2896d20cdf8a3b03e32043b2470d4350
Sejo Basic <sejoob@gmail.com>
2022-04-13 00:02:24 -0700

    Webpack setup final

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const webpack = require('webpack')
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
@@ -70,6 +72,8 @@ module.exports = {
         },
       }
     }),
+
+    new CleanWebpackPlugin()
   ],
 
   module: {
@@ -133,5 +137,10 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
   }
 }
