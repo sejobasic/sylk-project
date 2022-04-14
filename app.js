2e6e1d116f9569f2d21707689673a91882dd9ea1
Sejo Basic <sejoob@gmail.com>
2022-04-14 00:29:12 -0700

    Built Basic structure and routes using pug

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const port = 3000
 const path = require('path')
 
 app.set('views', path.join(__dirname, 'views'))
-app.set('view-engine', 'pug')
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('pages/home')
+})
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
+})
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections')
 })
 
 app.listen(port, () => {
